3602bf9daba77c4e1eb8fd869d89851cf332f016
shaunpurcell <23355573@studentmail.ul.ie>
2024-11-14 13:23:10 +0000

    Update NetPay.java

--- NetPay.java
+++ NetPay.java
@@ -5,7 +5,7 @@ public class NetPay extends PaySlip{
     private double prsiRate;
     //4.1%
     private double healthInsuranceRate;
-    // 1600 euro /12 for monthly
+    // 1600 euro /12 for monthly (based on average health insurance rate in Ireland)
     private double uscRate;
     // if salary greater than 12,012 tax is 0.5%
     // < than 25760 is 2%
