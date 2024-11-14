ccea5a1702bce1becdcdc078785cd5feea1a67d8
shaunpurcell <23355573@studentmail.ul.ie>
2024-11-14 13:50:55 +0000

    Update NetPay.java

--- NetPay.java
+++ NetPay.java
@@ -1,17 +1,17 @@
 // Shaun
 public class NetPay extends PaySlip{
-    private double salary;
+    private double grossPay;
     //salary called from CSV into one of employees then called to here, all deductions then done off of that
     private double prsiRate = 4.1%
     //4.1%
     private double healthInsuranceRate = 1600/12
     // 1600 euro /12 for monthly (based on average health insurance rate in Ireland)
     private double uscRate;
-    if (salary < 12012) {
+    if (grossPay < 12012) {
     uscRate = 0.005 }
-    else if (salary < 25760) {
+    else if (grossPay < 25760) {
     uscRate = 0.02 }
-    else if (salary < 70044) {
+    else if (grossPay < 70044) {
     uscRate = 0.04 }
     else {
     uscRate = 0.08 }
@@ -19,17 +19,17 @@ public class NetPay extends PaySlip{
     // <12012 > 25760 is 2%
     // >25760 <70,044 is 4%
     //greater than 70,044 is 8%
-    private double unionFeesRate = salary * 0.992
+    private double unionFeesRate = grossPay * 0.992
     // 0.8% of salary or 80c for every 100 euro earned
     private static double calculateIncomeTax {
         double incomeTaxRate = 0.0;
         double threshold = 42000
 
-        if (salary < threshold) {
+        if (grossPay < threshold) {
             incomeTaxRate = 0.20; }
         else {
             double lowerTax = threshold * 0.20;
-            double upperTax = (salary - threshold) * 0.40;
+            double upperTax = (grossPay - threshold) * 0.40;
             incomeTaxRate = lowerTax + upperTax ; 
         }
     // 20% for everything below 42,000, 40% for everything above
