3c55ceaac2b01947c05c991bea297d13dc89531e
Chobnar <conormaddock1@gmail.com>
2024-11-14 13:59:17 +0000

    Update PayrollMenu.java

--- PayrollMenu.java
+++ PayrollMenu.java
@@ -1,18 +1,39 @@
 import java.util.Scanner;
-//implements payroll system--Conor
+//Conor
 public class PayrollMenu {
-    Scanner in = new Scanner();
-
+    
+       
     //implements scanner
+    Scanner in = new Scanner(System.in);
     //asks for user login (employee, admin, hr)
-    //if(employee)
+    System.out.println("(0) Employee");
+    System.out.println("(1) Admin");
+    System.out.println("(2) Human Resources");
+
+    int user_type = in.nextInt();
+    if(user_type != 0 && user_type != 1 && user_type != 2){
+        throw new ArithmeticException("Please enter 0, 1 or 2");
+    }
+    
+    if(user_type == 0){
     //1. access personal details
     //2. access this months payslip
-    //3. access historic payslip
-    //if(admin)
+    //3. access historic payslip 
+        
+    }
+ 
+    
+    if(user_type == 1){
     //1.create employee
-    //if(human resources)
+        
+    }
+   
+    
+    if(user_type == 2){
     //1.implement promotion
+        
+    }
+
 
 
     //admin: login, add employee
