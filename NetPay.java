881d18130a610f2ee0b4b3d597cf1e83b83e9bad
shaunpurcell <23355573@studentmail.ul.ie>
2024-11-14 13:15:51 +0000

    Update NetPay.java

--- NetPay.java
+++ NetPay.java
@@ -3,11 +3,18 @@ public class NetPay extends PaySlip{
     private double salary;
     //salary called from CSV into one of employees then called to here, all deductions then done off of that
     private double prsiRate;
+    //4.1%
     private double healthInsuranceRate;
+    //
     private double uscRate;
+    // if salary greater than 12,012 tax is 0.5%
+    // < than 25760 is 2%
+    // <70,044 is 4%
+    //greater than 70,044 is 8%T
     private double unionFeesRate;
+    // 0.8% of salary or 80c for every 100 euro earned
     private double incomeTaxRate;
-
+    // 20% for everything below 42,000, 40% for everything above
     public NetPay(Salary salary){
         double usc = salary/
         double prsi = salary/
