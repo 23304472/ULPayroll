142ed93fdc8f6c45118681fc23e9cfbfb4537af0
avaleyden <23364696@studentmail.ul.ie>
2024-11-14 13:59:12 +0000

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -12,10 +12,10 @@ public class Admin {
     
   //adds a new employee to the csv file
     public void addEmployee(Employee employee){
-       BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
+       BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true));
 
       //Puts the employee details in a line in csv file
-      String line = employee.getName() + "," + employee.getTitle() + "," + employee.getRole();
+      String line = employee.getName() + ", " + employee.getTitle() + ", " + employee.getRole();
 
       //writes the line into the csv file and goes to next line then closes the writer
       writer.write(line);
