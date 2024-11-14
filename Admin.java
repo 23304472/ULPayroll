de6d68c46622b684d3a835e8710dd19c05856a0e
23304472 <23304472@studentmail.ul.ie>
2024-11-14 12:28:49 +0000

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,9 +1,6 @@
 import java.util.ArrayList;
 
 public class Admin {
-    ArrayList<Employee> employees;
-
     public void addEmployee(Employee employee){
-        employees.add(employee);
-    }
+        //add to csv file
 }
