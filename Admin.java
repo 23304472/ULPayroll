43e9afa4da27d5e72d445f777849cbd4b3c0d836
avaleyden <23364696@studentmail.ul.ie>
2024-11-14 13:13:14 +0000

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,7 +1,12 @@
 //Ava 
+import java.io.BufferedWriter
+import java.io.FileWriter
 import java.util.ArrayList;
 
 public class Admin {
+  private String filePath;
+
+    
     public void addEmployee(Employee employee){
         //add to csv file
 }
