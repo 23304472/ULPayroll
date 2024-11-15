79374a6e7c5a0c99c561cafbd5c2fbb5bcf135df
shaunpurcell <23355573@studentmail.ul.ie>
2024-11-15 16:05:35 +0000

    Update NetPay.java

--- NetPay.java
+++ NetPay.java
@@ -1,41 +1,59 @@
 // Shaun
-public class NetPay extends PaySlip{
+public class NetPay extends Payslip {
     private double grossPay;
     //salary called from CSV into one of employees then called to here, all deductions then done off of that
-    private double prsiRate = 4.1%
-    //4.1%
-    private double healthInsuranceRate = 1600/12
-    // 1600 euro /12 for monthly (based on average health insurance rate in Ireland)
+    private double prsiRate = 0.041;                 //4.1%
+    private double healthInsuranceRate = 1600.0 / 12;  // 1600 euro a year (based on average health insurance rate in Ireland)
     private double uscRate;
-    if (grossPay < 12012) {
-    uscRate = 0.005 }
-    else if (grossPay < 25760) {
-    uscRate = 0.02 }
-    else if (grossPay < 70044) {
-    uscRate = 0.04 }
-    else {
-    uscRate = 0.08 }
-    // if salary less than 12,012 tax is 0.5%
-    // <12012 > 25760 is 2%
-    // >25760 <70,044 is 4%
-    //greater than 70,044 is 8%
-    private double unionFeesRate = grossPay * 0.992
-    // 0.8% of salary or 80c for every 100 euro earned
-    private static double calculateIncomeTax {
-        double incomeTaxRate = 0.0;
-        double threshold = 42000
+    private double unionFeesRate = 0.008;            // 0.8% of salary or 80c for every 100 euro earned
+    private double incomeTax;
 
-        if (grossPay < threshold) {
-            incomeTaxRate = 0.20; }
-        else {
-            double lowerTax = threshold * 0.20;
-            double upperTax = (grossPay - threshold) * 0.40;
-            incomeTaxRate = lowerTax + upperTax ; 
+    public void calculateUscRate() {
+        if (grossPay < 12012) {
+            uscRate = 0.005;             // if gross pay less than 12,012 tax is 0.5%
+        } else if (grossPay < 25760) {
+            uscRate = 0.02;              // <12012 > 25760 is 2%
+        } else if (grossPay < 70044) {
+            uscRate = 0.04;              // >25760 <70,044 is 4%
+        } else {
+            uscRate = 0.08;              //greater than 70,044 is 8%
         }
-    // 20% for everything below 42,000, 40% for everything above
-    private NetPay(Salary salary){
-        double usc = salary/
-        double prsi = salary/
-        double
+    }
+
+    public void calculateIncomeTax() {
+        double threshold = 42000;
+        if (grossPay <= threshold) {
+            incomeTax = grossPay * 0.20; // Apply 20% for the entire grossPay
+        } else {
+            double lowerTax = threshold * 0.20; // Apply 20% for the first 42,000
+            double upperTax = (grossPay - threshold) * 0.40; // Apply 40% for the rest
+            incomeTax = lowerTax + upperTax;
+        }
+    }
+    private double netPay;
+    public void calculateNetPay() {
+        // 20% for everything below 42,000, 40% for everything above
+        netPay = grossPay
+                - (grossPay * prsiRate)
+                - (grossPay * uscRate)
+                - (grossPay * unionFeesRate)
+                - (grossPay * incomeTax)
+                - healthInsuranceRate;
+    }
+    // getters and setters
+    public double getGrossPay() {
+        return grossPay;
+    }
+    public void setGrossPay(double grossPay) {
+        this.grossPay = grossPay;
+    }
+    public double getNetPay() {
+        return netPay;
+    }
+    public double getIncomeTax(){
+        return incomeTax;
+    }
+    public double getUscRate(){
+        return uscRate;
     }
 }
