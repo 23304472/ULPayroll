39d807e45b78fccd128935a6dd0bccb40b82a6bb
23304472 <23304472@studentmail.ul.ie>
2024-11-16 11:33:47 +0000

    Update HourlyEmployee.java

--- HourlyEmployee.java
+++ HourlyEmployee.java
@@ -1,13 +1,48 @@
 //Emma
 public class HourlyEmployee extends Employee{
 
-    public HourlyEmployee(String name, String role){
-        super(name, role);
+    private double hours;
+
+    public HourlyEmployee(){
+    }
+
+    public HourlyEmployee(int id, String name, String title, double hours){
+        super(id, name, title);
+        this.hours = hours;
+    }
+
+    public int getId(){
+        return super.getId();
+    } public void setId(int id){
+        super.setId(id);
+    }
+
+    public String getName(){
+        return super.getName();
+    } public void setName(String name){
+        super.setName(name);
+    }
+
+    public String getTitle(){
+        return super.getTitle();
+    } public void setTitle(String title){
+        super.setTitle(title);
+    }
+
+    public double getHours(){
+        return hours;
+    } public void setHours(int hours){
+        this.hours = hours;
+    }
+
+    public String getHourlyRate(){
+        //calls csv read hourly file method
     }
 
     //gets hourly salary from hourly csv file
     @Override
     public String toString(){
-        return super.toString() + "Employee is paid an hourly salary";
+        return super.toString() +
+                "Salary type: Hourly" + "\n";
     }
 }
