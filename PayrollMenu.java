892c36543c55ba7a999c7199ceb4400618731e89
Chobnar <conormaddock1@gmail.com>
2024-11-17 18:49:27 +0000

    Update PayrollMenu.java

--- PayrollMenu.java
+++ PayrollMenu.java
@@ -1,44 +1,53 @@
 import java.util.Scanner;
 //Conor
 public class PayrollMenu {
+	//Employee arraylist
+	private static ArrayList<Employee> employees = new ArrayList<>();    
     
     public static void main(String[] args) {   
+    	
     //implements scanner
     Scanner in = new Scanner(System.in);
+    
     //asks for user login (employee, admin, hr)
     System.out.println("(0) Employee");
     System.out.println("(1) Admin");
     System.out.println("(2) Human Resources");
 
+    //takes input
     int user_type = in.nextInt();
-    if(user_type != 0 && user_type != 1 && user_type != 2){
-        throw new ArithmeticException("Please enter 0, 1 or 2");
+    
+    //handles illegal inputs
+    if(userType < 0 || userType > 2){
+    	throw new IllegalArgumentException("Please enter 0, 1, or 2");
     }
     
+    //Employee 
     if(user_type == 0){
-    //1. access personal details
-    //2. access this months payslip
-    //3. access historic payslip 
-        
-    }
- 
-    
+    	    System.out.println("Employee Menu:");
+          System.out.println("(0) Access Personal Details");
+          System.out.println("(1) View Current Month's Payslip");
+          System.out.println("(2) View Historic Payslips");
+
+          int choice = in.nextInt();
+          if (choice == 0) {
+             System.out.println(Employee.toString());   //Getter methods or toString method?
+          } else if (choice == 1) {
+             
+          } else if (choice == 2) {
+             
+          } else {
+              System.out.println("Invalid");
+          }
+    //Admin
     if(user_type == 1){
-    //1.create employee
-        
-    }
-   
     
+    }
+    
+    //Human Resources
     if(user_type == 2){
-    //1.implement promotion
+      
     }
-    
-    
+    }
 }
-
-
-
-    //admin: login, add employee
-    //employee - view details, payslips, historic payslips
-    //human resources - promotions, send message to employee for confirmation
 }
