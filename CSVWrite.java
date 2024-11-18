6d0f24723ec72cae9ab8c72dd3870754a7abd6ea
23304472 <23304472@studentmail.ul.ie>
2024-11-18 22:48:17 +0000

    Update CSVWrite.java

--- CSVWrite.java
+++ CSVWrite.java
@@ -1,2 +1,54 @@
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class CSVWrite {
+    PrintWriter writer;
+
+    public CSVWrite(String filename) throws FileNotFoundException{
+        writer = new PrintWriter(filename);
+    }
+
+    public void addEmployee(int id, String name, String title, LocalDate dateAdded){
+        String employeeInfo = id + ", " + name + ", " + title + ", " + dateAdded;
+        java.io.File file = new java.io.File("Employees.csv");
+
+        try (PrintWriter writer = new PrintWriter (new FileWriter("Employees.csv", true))){
+            if(file.length() == 0) {
+                String header = "ID, Name, Title, Date Added";
+                header += "\n";
+                writer.write(header);
+            }
+            writer.println(employeeInfo);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void addPayslip(double grossPay, double deductions, double netPay){
+        String paySlipInfo = grossPay + ", " + deductions + ", " + netPay;
+
+        java.io.File file = new java.io.File("Payslips.csv");
+
+        try(PrintWriter writer = new PrintWriter (new FileWriter("Payslips.csv", true))){
+            if(file.length() == 0) {
+                String header = "Gross Pay, Deductions, Net Pay";
+                header += "\n";
+                writer.write(header);
+            }
+            writer.println(paySlipInfo);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+    //write to employee file
+    //write to payslip file
+    //set method should change value in csv file
 }
