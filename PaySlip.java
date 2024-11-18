0aaddfc478c402d8e5bf0b10a00d3f5e35d719c9
shaunpurcell <23355573@studentmail.ul.ie>
2024-11-18 15:43:34 +0000

    Update PaySlip.java

--- PaySlip.java
+++ PaySlip.java
@@ -5,7 +5,16 @@ public class PaySlip {
     //stores all payslips to csv file
 
     //employee receives payslip given employee and date
-    public String toString(){
-        //prints employee name, role, taxes,, net pay
+@Override
+    public String toString() {
+        return "PaySlip Details:\n"
+                + "Employee ID: " + employee.getId() + "\n"
+                + "Name: " + employee.getName() + "\n"
+                + "Title: " + employee.getTitle() + "\n"
+                + "Date: " + date + "\n"
+                + "Gross Pay: €" + netPayDetails.getGrossPay() + "\n"
+                + "Income Tax: €" + netPayDetails.getIncomeTax() + "\n"
+                + "USC Rate: " + (netPayDetails.getUscRate() * 100) + "%\n"
+                + "Net Pay: €" + netPayDetails.getNetPay() + "\n";
     }
 }
