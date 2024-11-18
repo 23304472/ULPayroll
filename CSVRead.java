cec840f016ca50d54ba5acb5e0aaa19a84570e7e
23304472 <23304472@studentmail.ul.ie>
2024-11-18 22:46:37 +0000

    Update CSVRead.java

--- CSVRead.java
+++ CSVRead.java
@@ -1,7 +1,8 @@
 import java.io.*;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.*;
 
 public class CSVRead {
     Scanner input; //scanner object for reading file
@@ -11,81 +12,33 @@ public class CSVRead {
     } // opens file for reading
 
     public String[] getValues(){
-        String[] tokens;
-        String line = input.nextLine();
-        tokens = line.split(",");
+        String[] tokens; //new array
+        String line = input.nextLine(); //skips header line
+        tokens = line.split(","); //tokens = array of values in current line
         return tokens;
     }
 
     public ArrayList<String> readToList(){
-        ArrayList<String> allData = new ArrayList<String>(); //Arraylist holds all lines
-        while(input.hasNext()){
+        ArrayList<String> allData = new ArrayList<String>(); //new arraylist of strings
+        while(input.hasNext()){ //while there is more data in file
             allData.add(input.nextLine()); //add line to list
         }
-        return allData; //returns all lines
+        return allData; //returns all data in file as an arraylist
     }
 
-    public ArrayList<Employee> readEmployeeList(){
-        int lineNo = 0;
-        ArrayList<Employee> employees = new ArrayList<Employee>();
-        String header = input.nextLine();
-
-        while(input.hasNext()){
+    //assumes file is of form tokens[0] = title, tokens[1] = scale number(years), tokens[2] = salary rate
+    public String readSalary(Employee employee){
+        String line  = input.nextLine(); //skips header line
+        while(input.hasNext()){ //while there is more data in file
             String[] tokens = getValues();
-            employees.add(new Employee(tokens));
+            if((tokens[0].equals(employee.getTitle())) && (Integer.parseInt(tokens[1]) == (ChronoUnit.YEARS.between(employee.getDateAdded(), LocalDate.now())))){
+                //check if tokens[0] (title name) = employees title
+                // check if service year value matches the difference in years between when employee was created and the time now
+                return tokens[2]; //return tokens[2] (salary value)
+            }
         }
         input.close();
-        return employees;
+        return null;
     }
 
-    public ArrayList<String> readFullTimeSalary(){
-        int lineNo = 0;
-        ArrayList<String> fullTimeSalaries = new ArrayList<String>();
-        String header = input.nextLine();
-
-        while(input.hasNext()){
-            String[] tokens = getValues();
-            fullTimeSalaries.add(new String(tokens));
-        }
-        input.close();
-        return fullTimeSalaries;
-    }
-
-    public ArrayList<String> readPartTimeSalaries(){
-        int lineNo = 0;
-        ArrayList<String> partTimeSalaries = new ArrayList<String>();
-        String header = input.nextLine();
-
-        while(input.hasNext()){
-            String[] tokens = getValues();
-            partTimeSalaries.add(new String(tokens));
-        }
-        input.close();
-        return partTimeSalaries;
-    }
-
-    public ArrayList<String> readPaySlips(){
-        int lineNo = 0;
-        ArrayList<String> paySlipDetails = new ArrayList<String>();
-        String header = input.nextLine();
-
-        while(input.hasNext()){
-            String[] tokens = getValues();
-            paySlipDetails.add(new String (tokens));
-        }
-        input.close();
-        return paySlipDetails;
-    }
-
-    public ArrayList<String> readHourlyRates(){
-        int lineNo = 0;
-        ArrayList<String> hourlyRates = new ArrayList<String>();
-        String header = input.nextLine();
-        while(input.hasNext()){
-            String[] tokens = getValues();
-            hourlyRates.add(new String(tokens));
-        }
-        input.close();
-        return hourlyRates;
-    }
 }
