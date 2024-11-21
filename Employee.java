7a9eceb9ee622977f614cf63a90f5e824ec46cc5
Chobnar <conormaddock1@gmail.com>
2024-11-21 21:41:25 +0000

    Update Employee.java
    reverted back after accidentally commiting to the wrong file

--- Employee.java
+++ Employee.java
@@ -1,138 +1,50 @@
-import java.util.Scanner;
-//Conor
-public class PayrollMenu {
+import java.io.FileNotFoundException;
+import java.time.LocalDate;
 
-	private static boolean isDigitsOnly(String str) {
-		for (char c : str.toCharArray()) {
-			if (!Character.isDigit(c)) {
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	public static void main(String[] args) {   
+public class Employee {
 
+    protected int id;
+    protected String name;
+    protected String title;
+    protected LocalDate dateAdded;
+    CSVWrite csv = new CSVWrite("Employees.csv"); //
 
-		//implements scanner
-		Scanner in = new Scanner(System.in);
+    public Employee(int id, String name, String title) throws FileNotFoundException {
+        this.name = name;
+        this.title = title;
+        LocalDate dateAdded = LocalDate.now(); //stores date employees is added
+        csv.addEmployee(id, name, title, dateAdded); //adds new employee to csv file
+    }
 
-		//asks for user login (employee, admin, hr)
-		System.out.println("(0) Employee");
-		System.out.println("(1) Admin");
-		System.out.println("(2) Human Resources");
+    public int getId(){
+        return id;
+    } public void setId(int id){
+        this.id = id;
+    }
 
+    public String getName(){
+        return name;
+    } public void setName(String name){
+        this.name = name;
+    }
 
-		//takes input
-		int user_type = in.nextInt();
+    public String getTitle(){
+        return title;
+    }public void setTitle(String role){
+        this.title = role;
+    }
 
-		//handles illegal inputs
-		if(user_type < 0 || user_type > 2){
-			throw new IllegalArgumentException("Please enter 0, 1, or 2");
-		}
+    public LocalDate getDateAdded(){
+        return dateAdded;
+    }
 
-		//Employee 
-		if(user_type == 0){
-			System.out.println("Employee Menu:");
-			System.out.println("(0) Access Personal Details");
-			System.out.println("(1) View Current Month's Payslip");
-			System.out.println("(2) View Historic Payslips");
-			System.out.println("(3) Back");
+    public String toString(){
+        return "Employee: " + id + "\n" +
+                "Name: " + name + "\n" +
+                "Title: " + title + "\n" +
+                "Hours worked: " + "\n";
+    }
 
-			//password system
-
-			int choice = in.nextInt();
-			if (choice == 0) {
-				System.out.println(emp.toString());     
-			} 
-			else if (choice == 1) {
-
-			} else if (choice == 2) {
-
-			} else {
-				//Didn't choose an action
-				System.out.println("Returning");  
-			}
-		}
-
-		
-		
-		
-		//Admin
-		if(user_type == 1){
-			System.out.println("Admin Menu:");
-			System.out.println("(0) Add Employee");
-			System.out.println("(1) Back");
-
-			int choice = in.nextInt();
-			if(choice == 0) { 
-				//adding new employee
-				//enter id
-				System.out.println("Please enter employee id");
-				String empId = in.next();
-				if (empId.length() == 5 && isDigitsOnly(empId)) {
-					System.out.println("Employee id is " + empId);
-				} else {
-					System.out.println("Invalid");
-				}
-			}
-
-			//enter name
-			System.out.println("Please enter employee name");
-			String empName = in.next();
-			System.out.println("Employee name is " + empName);
-
-			else {
-				System.out.println("Returning");          
-			}
-		}
-
-
-
-
-
-		//Human Resources
-		if(user_type == 2) {
-			System.out.println("Human Resources Menu:");
-			System.out.println("(0) Implement Promotion for full-time staff");
-			System.out.println("(1) Back");
-
-			int choice = in.nextInt();
-			if(choice == 0) {
-				//Promotion for full time staff
-			} else {
-				System.out.println("Returning");
-			}
-
-		}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	}
-
+    //get hourly employee salary should read from one data file, get full-time employee salary reads from another
+    //make excel file with salary info, use scanner to read and find salaries
 }
