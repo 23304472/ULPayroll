df44ccffcb5f9628b88ecd49ecb0013155f6acc4
23304472 <23304472@studentmail.ul.ie>
2024-11-25 21:06:49 +0000

    Update CSVRead.java

--- CSVRead.java
+++ CSVRead.java
@@ -18,7 +18,7 @@ public class CSVRead {
         return tokens;
     }
 
-    public ArrayList<String> readToList(){
+    public ArrayList<String> readHistoricPayslips(){
         ArrayList<String> allData = new ArrayList<String>(); //new arraylist of strings
         while(input.hasNext()){ //while there is more data in file
             allData.add(input.nextLine()); //add line to list
@@ -32,26 +32,37 @@ public class CSVRead {
         while(input.hasNext()){ //while there is more data in file
             String[] tokens = getValues();
             if((tokens[0].equals(employee.getTitle())) && (Integer.parseInt(tokens[1]) == (ChronoUnit.YEARS.between(employee.getDateAdded(), LocalDate.now())))){
-                //check if tokens[0] (title name) = employees title
-                // check if service year value matches the difference in years between when employee was created and the time now
+                //check if tokens[0] (title column) = employees title
+                // check if scale point value matches the difference in years between when employee was created and the time now
                 return tokens[2]; //return tokens[2] (salary value)
             }
         }
-        input.close();
         return null;
     }
-    
+
     public String[] readEmployeeDetails(Employee employee){
-            String header = input.nextLine();
-            while(input.hasNext()){
-                String[] tokens = getValues();
-                String id = String.valueOf(employee.getId());
-                if(tokens[0].equals(id)){
-                    return tokens;
-                }
+        String header = input.nextLine();
+        while(input.hasNext()){
+            String[] tokens = getValues();
+            int id = employee.getId();
+            if(tokens[0].equals(id)){
+                return tokens;
             }
-            return new String[0];
         }
-   
+        return new String[0];
+    }
+
+    public String[] getMostRecentPayslip(Employee employee){
+        String header = input.nextLine();
+
+        if(!input.hasNext()){
+            String[] currentTokens = getValues();
+            return currentTokens;
+        }
+    }
+
+    public void close(){
+        input.close();
+    }
 
 }
